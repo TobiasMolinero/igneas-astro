4624159cb180bdb024d7fc706d6f636c9c84a86b
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-04-15 13:54:23 -0300

    cambios en members.ts

--- src/consts/members.ts
+++ src/consts/members.ts
@@ -1,4 +1,7 @@
 import { type Member} from "../types/member";
+import foto_jessica from '/public/images/foto-jesi-sinfondo.webp';
+import foto_victoria from '/public/images/foto-viki-sinfondo.webp';
+import foto_majo from '/public/images/foto-majo-sinfondo.webp';
 
 export const MIEMBROS: Member[] = [
     {
@@ -6,7 +9,7 @@ export const MIEMBROS: Member[] = [
         name: 'Jessica Gomez Leccese',
         desc: 'Lic. en Artes Plásticas. Especialista en esculturas. Diplomada en liderazgo politico. Miembro fundador de IGNEAS.',
         position: 'Presidenta de IGNEAS',
-        image: '/public/images/foto-jesi-sinfondo.webp',
+        image: foto_jessica.src,
         alt: 'Imagen de Jessica, miembro fundador y Presidente de la Cooperativa'
     },
     {
@@ -14,7 +17,7 @@ export const MIEMBROS: Member[] = [
         name: 'Maria Victoria Herrero Jaime',
         desc: 'Lic. en Artes Plásticas especializada en grabado y posgrado en Arte Digital. Miembro fundador de IGNEAS.',
         position: 'Vicepresidenta de IGNEAS',
-        image: '/public/images/foto-viki-sinfondo.webp',
+        image: foto_victoria.src,
         alt: 'Imagen de Victoria, miembro fundador y Vicepresidenta de la Cooperativa'
     },
     {
@@ -22,7 +25,7 @@ export const MIEMBROS: Member[] = [
         name: 'María José Mazzocato',
         desc: 'Lic. en Relaciones Internacionales. Especialista en terrorismo, defensa y estrategia militar. Docente Academica. Miembro fundador de IGNEAS.',
         position: 'Secretaria General',
-        image: '/public/images/foto-majo-sinfondo.webp',
+        image: foto_majo.src,
         alt: 'Imagen de Maria Jose, miembro fundador y Secretaria General de la Cooperativa'
     }
 ]
